18a8c4e1e4338719ee58a6f5857a10ec6fb2b6d4
sosukii <sweta4ca@mail.ru>
2021-05-06 22:09:21 +0700

    add 2 components

---
+++ src/CardInfo.js
@@ -0,0 +1,13 @@
+function Cardinfo() {
+
+    return(
+        <div>
+
+            <div>have a good day honey!!! </div>
+
+        </div>
+    )
+
+}
+
+export default Cardinfo
---
+++ src/Myname.js
@@ -0,0 +1,6 @@
+function Myname() {
+    return(
+        <h3>My name is sosukii</h3>
+    );
+}
+export default Myname
